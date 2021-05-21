c571c97ddd6ebfb5f096df463a56d7fc1e5f5032
Patricia <aaraujo.patricia@gmail.com>
2021-05-20 23:41:55 -0300

    desenvolvendo as funções de validação

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+/*
+Case sensitive = reconhece maiuscula e minuscula
+por tag: getElementeByTagName()
+por Id:getElementeById()
+por Nome: getElementeByTagName()
+por classe: getElementeByTagClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('input#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txt = document.querySelector ('#txtNome')
+    if(nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome invalido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome Valido'
+        txtNome.style.color = 'green'
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector ('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf ('.') == -1) {
+        txtEmail.innerHTML = 'E-mail invalido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail Valido'
+        txtEmail.style.color = 'green'
+    }
+}
+    
+function validaAssunto() {
+    
+    alert(assunto)
+    
+    /*let txtAssunto = document.querySelector('#txtAssunto')*/
+    
+    /*if (assunto.value.length >= 100) {
+        alert ('entrou no else')
+        /*txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        /*txtAssunto.style.display = 'none'
+        assuntoOk = true
+        alert ('entrou no else')
+    }*/
+}
+         
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert ('Formulário enviado com sucesso!')
+    } else {
+        alert ('Preencha o formulário corretamente antes de enviar...')
+    }
+}
+    
